80c12c1f83302d38518e7004db4a1d1f3c30cf4b
Kishanu <gopi.sarkar93@gmail.com>
2020-10-09 18:46:44 +0530

    hello world, hungry or not version

--- Hello_World.c
+++ Hello_World.c
@@ -4,5 +4,16 @@ int main(){
 
 printf("Heloo World");
 
+char hungry_flag;
+printf("Are you hungry");
+scanf("%c",&hungry_flag);
+
+if(hungry_flag=='Y'){
+	prinf("eat some food");
+}
+else{
+	printf("Chill out");
+}
+
 return 0;
 }
